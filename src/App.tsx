7ed23246d05f50746f5185515a2d8b006e9ba2ad
ElenaLj <elenaljubijankic@gmail.com>
2025-06-30 09:14:48 +0200

    remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import { useState } from 'react';
 function App() {
   let [counter, add] = useState(0);
   function clickMe(){
-      // console.log("button clicked")
       add(counter+1)
       console.log(counter)
     }
 
 
   return (
-    <> 
+    <>
+     <> 
+
     <button onClick={clickMe}>current count: {counter}</button>
     </>
+    <div></div>  
+    </>
+   
   )
   
 }
