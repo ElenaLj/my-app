94a326c6f3dead546542e8e3104d8d23bcddabb4
ElenaLj <elenaljubijankic@gmail.com>
2025-07-02 15:33:03 +0200

    add conditional css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 
 import './App.css';
 import { useState } from 'react';
+import clsx from 'clsx';
 
 function App() {
   let [counter, add] = useState(0);
   function clickMe(){
       add(counter+1)
       console.log(counter)
-    }
+  }
 
+  // const kitty = {
+  //   name: "Milly",
+  //   gender: "F"
+  // }
+
+let [clicked, setState] = useState(false) 
+
+  function handleClick(){
+    setState(clicked = true)
+  }
 
   return (
     <>
-     <> 
-
     <button onClick={clickMe}>current count: {counter}</button>
-    </>
     <div></div>  
+
+    <button className={'box' + ' ' + clsx(clicked ? "milly" : null)} onClick={handleClick} value={"Milly"}>Milly</button>
+    <button className={'box' + ' ' + clsx(clicked ? "merlino" : null)} onClick={handleClick} value={"Merlino"}>Merlino</button>
+    <button className={'box' + ' ' + clsx(clicked ? "miffie" : null)} onClick={handleClick} value={"Miffie"}>Miffie</button>
     </>
-   
   )
   
 }
